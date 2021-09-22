2ad78d45955affe519ac1db01767a96b01e6d6ee
TWAllison <tylerallison55@gmail.com>
2021-09-21 23:04:20 -0600

    save searched city to local storage in an array

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,5 +1,4 @@
 var weatherEl = $("#current-weather")
-var previousSearch = JSON.parse(localStorage.getItem("City Name")) || [];
 var fiveDayEl = $("#five-day");
 var fiveDayHeaderEl = $("#five-day-header");
 
@@ -8,17 +7,27 @@ var apiKey = "a6b61b1b7f92f9c968b0e70a23502785"
 
 
 
-  
 
-$("#search-btn").on("click", function() {  // listen for click on search btn then takes sibling data value (input-form) and sets value to cityName
+
+$("#search-btn").on("click", function () {  // listen for click on search btn then takes sibling data value (input-form) and sets value to cityName
     if (!$(this).siblings("input").val()) {
         alert("Enter a city Name")
         return
     } else {
         var cityName = $(this).siblings("input").val()
+        //test save function here
+        if (localStorage.getItem("city") === null) {
+            localStorage.setItem("city",'[]');
+        }
+    
+        var searchHistory = JSON.parse(localStorage.getItem("city"));
+        searchHistory.push(cityName);
+    
+        localStorage.setItem("city", JSON.stringify(searchHistory));
     }
 
     fetchWeatherData(cityName) // pass cityName created from form input to fetch function
+
 });
 
 var fetchWeatherData = function (cityName) {
@@ -35,68 +44,79 @@ var fetchWeatherData = function (cityName) {
             }
         });
 
-        saveSearch();
+
 
 };
 
-var saveSearch = function() {
-    localStorage.setItem("City Name", JSON.stringify(previousSearch)); // save to local
-}
+/*var saveSearch = function () {
+    var newSearch = $("#city-name").val();
+    if (localStorage.getItem("city") === null) {
+        localStorage.setItem("city",'[]');
+    }
 
-var currentWeather = function(weather) {
+    var searchHistory = JSON.parse(localStorage.getItem("city"));
+    searchHistory.push(newSearch);
+
+    localStorage.setItem("city", JSON.stringify(searchHistory));
+
+    saveSearch();
+}
+*/
+
+var currentWeather = function (weather) {
 
     var cityLat = weather.city.coord.lat
     var cityLon = weather.city.coord.lon
 
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`)
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                weatherEl.empty(); // clears current weather box to prevent more than one being created
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    weatherEl.empty(); // clears current weather box to prevent more than one being created
 
-                var cityTitle = document.createElement("h2")
-                cityTitle.textContent = weather.city.name + "(" + moment.unix(weather.list[0].dt).format("MMM DD, YYYY") + ")"; // take data returned at index 0 (current hour) 
+                    var cityTitle = document.createElement("h2")
+                    cityTitle.textContent = weather.city.name + "(" + moment.unix(weather.list[0].dt).format("MMM DD, YYYY") + ")"; // take data returned at index 0 (current hour) 
 
-                var forecastIcon = document.createElement("img")
-                forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png` )
+                    var forecastIcon = document.createElement("img")
+                    forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`)
 
-                cityTitle.append(forecastIcon) // append current weather icon to title
-                weatherEl.append(cityTitle) // append the title to the weatherEL on page (with added icon)
+                    cityTitle.append(forecastIcon) // append current weather icon to title
+                    weatherEl.append(cityTitle) // append the title to the weatherEL on page (with added icon)
 
-                var cityTemp = document.createElement("p")
-                cityTemp.textContent = "Temp: " + weather.list[0].main.temp + "°F"
-                weatherEl.append(cityTemp)
+                    var cityTemp = document.createElement("p")
+                    cityTemp.textContent = "Temp: " + weather.list[0].main.temp + "°F"
+                    weatherEl.append(cityTemp)
 
-                var cityWind = document.createElement("p")
-                cityWind.textContent = "Wind: " + weather.list[0].wind.speed + "MPH"
-                weatherEl.append(cityWind)
+                    var cityWind = document.createElement("p")
+                    cityWind.textContent = "Wind: " + weather.list[0].wind.speed + "MPH"
+                    weatherEl.append(cityWind)
 
-                var cityHumidity = document.createElement("p")
-                cityHumidity.textContent = "Humidity: " + weather.list[0].main.humidity + "%"
-                weatherEl.append(cityHumidity)
+                    var cityHumidity = document.createElement("p")
+                    cityHumidity.textContent = "Humidity: " + weather.list[0].main.humidity + "%"
+                    weatherEl.append(cityHumidity)
 
-                var cityUvText = document.createElement("p")
-                var cityUvContainer = document.createElement("span")
-                cityUv = data.current.uvi                   //create two elements to allow for a colored background 
-                cityUvContainer.textContent = cityUv 
-                cityUvText.textContent = "UV Index: "
-                cityUvText.append(cityUvContainer)
-                weatherEl.append(cityUvText)
+                    var cityUvText = document.createElement("p")
+                    var cityUvContainer = document.createElement("span")
+                    cityUv = data.current.uvi                   //create two elements to allow for a colored background 
+                    cityUvContainer.textContent = cityUv
+                    cityUvText.textContent = "UV Index: "
+                    cityUvText.append(cityUvContainer)
+                    weatherEl.append(cityUvText)
 
-                if (cityUv <= 4 ) {
-                    $(cityUvContainer).addClass("bg-success fs-4");  // use the UV index to determine the background color and assign boostrap class accordingly 
-                } else if (cityUv <=7 && cityUv > 4) {
-                    $(cityUvContainer).addClass("bg-warning fs-4");
-                } else if (cityUv <= 10 && cityUv > 7) {
-                    $(cityUvContainer).addClass("bg-danger fs-4");
-                }
-            })
-        }
-    });
+                    if (cityUv <= 4) {
+                        $(cityUvContainer).addClass("bg-success fs-4");  // use the UV index to determine the background color and assign boostrap class accordingly 
+                    } else if (cityUv <= 7 && cityUv > 4) {
+                        $(cityUvContainer).addClass("bg-warning fs-4");
+                    } else if (cityUv <= 10 && cityUv > 7) {
+                        $(cityUvContainer).addClass("bg-danger fs-4");
+                    }
+                })
+            }
+        });
 }
 
-var fiveDay = function(weather) {
+var fiveDay = function (weather) {
     fiveDayEl.empty();
     document.querySelector("#five-day-header").textContent = "5 Day Forcast";
 
@@ -113,23 +133,23 @@ var fiveDay = function(weather) {
         fiveDayCard.append(forecastDate);
 
         var forecastIcon = document.createElement("img")
-        forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.list[i].weather[0].icon}@2x.png` )
+        forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.list[i].weather[0].icon}@2x.png`)
         fiveDayCard.append(forecastIcon)
 
         var cityTemp = document.createElement("p")
-            cityTemp.textContent = "Temp: " + weather.list[i].main.temp + "°F"
-            fiveDayCard.append(cityTemp)
-            
-            var cityWind = document.createElement("p")
-            cityWind.textContent = "Wind: " + weather.list[i].wind.speed + "MPH"
-            fiveDayCard.append(cityWind)
+        cityTemp.textContent = "Temp: " + weather.list[i].main.temp + "°F"
+        fiveDayCard.append(cityTemp)
 
-            
-            var cityHumidity = document.createElement("p")
-            cityHumidity.textContent = "Humidity: " + weather.list[i].main.humidity + "%"
-            fiveDayCard.append(cityHumidity)
+        var cityWind = document.createElement("p")
+        cityWind.textContent = "Wind: " + weather.list[i].wind.speed + "MPH"
+        fiveDayCard.append(cityWind)
 
-            fiveDayEl.append(fiveDayCard)
+
+        var cityHumidity = document.createElement("p")
+        cityHumidity.textContent = "Humidity: " + weather.list[i].main.humidity + "%"
+        fiveDayCard.append(cityHumidity)
+
+        fiveDayEl.append(fiveDayCard)
 
     }
 
